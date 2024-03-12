45e8a9138a2b29ce5ac7294ea6d676be46e37a3f
Diego <69463084+diego-oniarti@users.noreply.github.com>
2024-03-12 02:48:20 +0100

    refactor

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ import numpy as np
 
 c = draw.Canvas()
 points = [
-    np.array([0, 0, 0]),
-    np.array([0, 0, 1]),
-    np.array([0, 1, 1]),
-    np.array([0, 1, 0]),
-    np.array([1, 0, 0]),
-    np.array([1, 0, 1]),
-    np.array([1, 1, 1]),
-    np.array([1, 1, 0]),
+    np.array([0, 0, 0, 1]),
+    np.array([0, 0, 1, 1]),
+    np.array([0, 1, 1, 1]),
+    np.array([0, 1, 0, 1]),
+    np.array([1, 0, 0, 1]),
+    np.array([1, 0, 1, 1]),
+    np.array([1, 1, 1, 1]),
+    np.array([1, 1, 0, 1]),
 ]
-points = [np.add(x, np.array([-0.5, -0.5, -0.5])) for x in points]
+points = [np.add(x, np.array([-0.5, -0.5, -0.5, 0])) for x in points]
 faces = [
     [0, 1, 2, 3],
     [4, 5, 6, 7],
@@ -34,57 +34,46 @@ def draw_line(x1, y1, x2, y2):
         # c.set(x, y+1)
 
 
+A1 = math.pi / 100
+A2 = math.pi / 81
+rot_mat = np.array([
+    [np.cos(A1) * np.cos(A2), np.cos(A1) * np.sin(A2), np.sin(A1), 0],
+    [-np.sin(A2), np.cos(A2), 0, 0],
+    [-np.sin(A1) * np.cos(A2), -np.sin(A2) * np.sin(A1), np.cos(A2), 0],
+    [0,0,0,0],
+])
+
+fov = math.pi / 3
+n = 1
+f = 10
+S = 1 / np.tan(fov / 2)
+proj = np.array([
+        [S, 0, 0, 0],
+        [0, S, 0, 0],
+        [0, 0, -f / (f - n), -1],
+        [0, 0, -f * n / (f - n), 0]
+])
+
+scale = 75
+T=0
 while True:
-    os.system('cls')
     c.clear()
 
-    A = math.pi / 100
-    points = map(lambda punto:
-                 np.matmul(punto, np.array([
-                     [np.cos(A), 0, np.sin(A)],
-                     [0, 1, 0],
-                     [-np.sin(A), 0, np.cos(A)]
-                 ])),
-                 points)
-
-    A = math.pi / 80
-    points = list(map(lambda punto:
-                      np.matmul(punto, np.array([
-                          [np.cos(A), np.sin(A), 0],
-                          [-np.sin(A), np.cos(A), 0],
-                          [0, 0, 1],
-                      ])),
-                      points))
-
-
-    def a(punto):
-        b = np.add(punto, np.array([0, 0, 3]))
-        return np.array([b[0], b[1], b[2], 1])
-
-    punti_dist = list(map(a,points))
-
-    fov = math.pi/3
-    n=1
-    f=10
-    S = 1/np.tan(fov/2)
-
-    def proj(p):
-        return np.matmul(p, np.array([
-                              [S, 0, 0, 0],
-                              [0, S, 0, 0],
-                              [0, 0, -f / ( f - n ) , -1],
-                              [0, 0, -f*n/(f-n), 0]
-                          ]))
-
-    punti_proj = list(map(proj,punti_dist))
+    points = list(map(lambda punto: np.matmul(punto, rot_mat), points))
+    punti_dist = list(map(lambda p: np.add(p,np.array([0, 0, 3, 0])), points))
+    punti_proj = list(map(lambda p: np.matmul(p,proj), punti_dist))
 
     for face in faces:
         for i in range(0, len(face)):
             a = punti_proj[face[i]]
             b = punti_proj[face[(i + 1) % len(face)]]
 
-            draw_line(a[0]/a[3]*50, a[1]/a[3]*50, b[0]/b[3]*50, b[1]/b[3]*50)
+            draw_line(a[0] / a[3] * scale, a[1] / a[3] * scale, b[0] / b[3] * scale, b[1] / b[3] * scale)
 
-    print(c.frame(-30, -30))
+    scale = 75 + np.sin(T/10)*25
+
+    os.system('cls')
+    print(c.frame(-55, -55))
 
     time.sleep(0.05)
+    T+=1
